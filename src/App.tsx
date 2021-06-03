3423e53fe9b9cafa8004bc9f7fad62347456f322
Kent Kawanaka <info@kriver-software.osaka>
2021-06-03 10:08:35 +0900

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as SubComponent from "./SubComponent";
+
+type State = {
+  counter: number;
+  subComponentState: SubComponent.State;
+};
+
+const initialState: State = {
+  counter: 0,
+  subComponentState: SubComponent.initialState,
+};
+type Action =
+  | {
+      type: "increment";
+    }
+  | { type: "decrement" }
+  | { type: "subComponent"; value: SubComponent.Action };
+
+const reducer: React.Reducer<State, Action> = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "subComponent":
+      return {
+        ...state,
+        subComponentState: SubComponent.reducer(
+          state.subComponentState,
+          action.value
+        ),
+      };
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const onPlusClicked = () => {
+    dispatch({ type: "increment" });
+  };
+  const onMinusClicked = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const subComponentDispatch = (a: SubComponent.Action) => {
+    dispatch({ type: "subComponent", value: a });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "1px solid black" }}>
+        <h1>main component</h1>
+        <div>current counter: {state.counter}</div>
+        <div>
+          <button onClick={onPlusClicked}>+</button>
+          <button onClick={onMinusClicked}>-</button>
+        </div>
+      </div>
+      <div style={{ border: "1px solid black" }}>
+        <h1>sub component</h1>
+        <SubComponent.View
+          dispatch={subComponentDispatch}
+          state={state.subComponentState}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/SubComponent.tsx
@@ -0,0 +1,59 @@
+export type State = {
+  name: string;
+  age: number;
+};
+export const initialState = {
+  name: "",
+  age: 0,
+};
+export type Action =
+  | {
+      type: "nameInput";
+      value: string;
+    }
+  | {
+      type: "ageInput";
+      value: number;
+    };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "nameInput":
+      return {
+        ...state,
+        name: action.value,
+      };
+    case "ageInput":
+      return {
+        ...state,
+        age: action.value,
+      };
+    default:
+      const _: never = action;
+      throw "error";
+  }
+};
+
+type Props = {
+  state: State;
+  dispatch: (a: Action) => void;
+};
+
+export const View: React.VFC<Props> = ({ state, dispatch }) => {
+  const onNameChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    dispatch({ type: "nameInput", value: e.target.value });
+  };
+  const onAgeChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    dispatch({ type: "ageInput", value: parseInt(e.target.value) });
+  };
+  return (
+    <div>
+      <div>
+        name: <input type="text" value={state.name} onChange={onNameChange} />
+      </div>
+      <div>
+        age: <input type="number" value={state.age} onChange={onAgeChange} />
+      </div>
+    </div>
+  );
+};
